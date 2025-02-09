51725718ecaf293762b6997d8f5e2a2b5e89be9c
Victor Nova <victor@borg.games>
2025-02-09 15:49:54 -0800

    basic worker

--- service/Worker.cs
+++ service/Worker.cs
@@ -1,19 +1,45 @@
 namespace Hash;
 
-public class Worker: BackgroundService {
-    readonly ILogger<Worker> log;
+using System.Net;
+using System.Net.Sockets;
 
-    public Worker(ILogger<Worker> log) {
-        this.log = log;
-    }
+using Borg.IO;
+using Borg.Threading;
+
+using static Environment;
+
+public class Worker: BackgroundService {
+    readonly ILoggerFactory logs;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
-        while (!stoppingToken.IsCancellationRequested) {
-            if (this.log.IsEnabled(LogLevel.Information)) {
-                this.log.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
+        var cacheDir =
+            new DirectoryInfo(GetFolderPath(SpecialFolder.LocalApplicationData))
+                .CreateSubdirectory("Borg")
+                .CreateSubdirectory(nameof(Hash))
+                .CreateSubdirectory("Cache");
 
-            await Task.Delay(1000, stoppingToken);
-        }
+        var blocksFile = cacheDir.File("cache.blocks");
+        if (!blocksFile.Exists)
+            await blocksFile.SetLength(64 * 1024L * 1024 * 1024, stoppingToken);
+        var indexFile = cacheDir.File("cache.index");
+
+        await using var cache = await BlockCache.CreateAsync(
+            indexPath: indexFile.FullName,
+            blocksPath: blocksFile.FullName,
+            blockSize: 16 * 1024,
+            this.logs.CreateLogger<BlockCache>(),
+            stoppingToken);
+        var validatingCache = new ValidatingBlockCache(cache);
+
+        using var listener = new TcpListener(IPAddress.Loopback, ContentStreamClient.DEFAULT_PORT);
+        var serverLog = this.logs.CreateLogger<TcpContentServer>();
+        var server = new TcpContentServer(listener, validatingCache, serverLog);
+        server.Start();
+
+        await TaskEx.TryDelay(Timeout.InfiniteTimeSpan, stoppingToken);
+    }
+
+    public Worker(ILoggerFactory logs) {
+        this.logs = logs ?? throw new ArgumentNullException(nameof(logs));
     }
 }
