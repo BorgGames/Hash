303430d72c0bb436105e31155328980cae7fc8cd
Victor Nova <victor@borg.games>
2025-02-10 11:14:16 -0800

    granular locking

--- src/BlockCache.cs
+++ src/BlockCache.cs
@@ -6,20 +6,25 @@ using Borg.Diagnostics;
 
 using Caching;
 
-public sealed class BlockCache: IBlockCache, IAsyncDisposable {
+using Microsoft.VisualStudio.Threading;
+
+public sealed class BlockCache: IBlockCache, System.IAsyncDisposable {
     public const int DEFAULT_BLOCK_SIZE = 16 * 1024;
+    public static readonly int PERF_TAG = 0xACED;
+    public static readonly int PERF_TAG_MASK = 0xFFFF;
 
     readonly BlockStorage storage;
     readonly Sieve<ContentHash> evictionStrategy;
     readonly ILogger log;
     readonly int blockSize = DEFAULT_BLOCK_SIZE;
+    readonly AsyncReaderWriterLock[] blockLocks;
 
     enum AccessPriority {
         RELEASER,
         ACQUIRER,
     }
 
-    readonly PrioritySemaphore<AccessPriority> accessLock = new();
+    readonly SemaphoreSlim indexLock = new(1);
     readonly Stopwatch hitRateReportStopwatch = Stopwatch.StartNew();
     long misses, hits;
 
@@ -34,30 +39,48 @@ public sealed class BlockCache: IBlockCache, IAsyncDisposable {
             throw new ArgumentOutOfRangeException(nameof(content), content.Length,
                                                   "Content is too large");
 
-        await this.accessLock.WaitAsync(AccessPriority.RELEASER, cancel).ConfigureAwait(false);
+        bool reportPerf = (hash.Lo4() & PERF_TAG_MASK) == PERF_TAG;
+        var perfLevel = reportPerf ? LogLevel.Debug : LogLevel.Trace;
+        var start = StopwatchTimestamp.Now;
+        await this.indexLock.WaitAsync(cancel).ConfigureAwait(false);
+        if (reportPerf)
+            this.log.LogDebug("write lock wait: {Microseconds:F0}us",
+                              start.Elapsed.TotalMicroseconds);
         try {
             int index;
-            var start = StopwatchTimestamp.Now;
+            var writeTime = start = StopwatchTimestamp.Now;
             if (this.evictionStrategy.Access(hash, out var evicted)) {
                 this.Evicted?.Invoke(this, evicted);
                 index = this.storage.BlockIndex(evicted);
                 if (index < 0)
                     throw new InvalidProgramException(
                         "Internal error: eviction strategy record does not match storage contents");
-                await this.storage.WriteAsync(index, content, hash, CancellationToken.None)
-                          .ConfigureAwait(false);
-                this.log.LogTrace("Set content[{Length}] for {Hash} at {Index}",
-                                  content.Length, hash, index);
+                if (reportPerf)
+                    this.log.LogDebug("eviction: {Microseconds:F0}us",
+                                      start.Elapsed.TotalMicroseconds);
+
+                start = StopwatchTimestamp.Now;
+                var entryLock = this.blockLocks[index % this.blockLocks.Length];
+                await using (await entryLock.WriteLockAsync(cancel))
+                    await this.storage.WriteAsync(index, content, hash, CancellationToken.None)
+                              .ConfigureAwait(false);
+
+                this.log.Log(
+                    perfLevel, "Set content[{Length}] for {Hash} at {Index} in {Microseconds:F0}us",
+                    content.Length, hash, index, start.Elapsed.TotalMicroseconds);
             } else {
 #if DEBUG
                 index = this.storage.BlockIndex(hash);
-                this.log.LogTrace("Content[{Length}] for {Hash} is already at {Index}",
-                                  content.Length, hash, index);
+
+                this.log.Log(perfLevel,
+                             "Content[{Length}] for {Hash} is already at {Index} in {Microseconds:F0}us",
+                             content.Length, hash, index, start.Elapsed.TotalMicroseconds);
                 Debug.Assert(index >= 0);
-                return start.Elapsed;
+                return writeTime.Elapsed;
 #else
-                this.log.LogTrace("Content[{Length}] for {Hash} is already in cache",
-                                  content.Length, hash);
+                this.log.Log(perfLevel,
+                             "Content[{Length}] for {Hash} is already in cache in {Microseconds:F0}us",
+                             content.Length, hash, start.Elapsed.TotalMicroseconds);
                 return TimeSpan.Zero;
 #endif
             }
@@ -65,7 +88,7 @@ public sealed class BlockCache: IBlockCache, IAsyncDisposable {
             this.Available?.Invoke(this, hash, content.Span);
             return start.Elapsed;
         } finally {
-            this.accessLock.Release();
+            this.indexLock.Release();
         }
     }
 
@@ -75,33 +98,60 @@ public sealed class BlockCache: IBlockCache, IAsyncDisposable {
         ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(offset, int.MaxValue);
         int offset32 = (int)offset;
 
-        await this.accessLock.WaitAsync(AccessPriority.ACQUIRER, CancellationToken.None)
-                  .ConfigureAwait(false);
+        bool reportPerf = (hash.Lo4() & PERF_TAG_MASK) == PERF_TAG;
+        var start = StopwatchTimestamp.Now;
+        await this.indexLock.WaitAsync(cancel).ConfigureAwait(false);
+        bool haveLock = true;
+        if (reportPerf)
+            this.log.LogDebug("read lock wait: {Microseconds:F0}us",
+                              start.Elapsed.TotalMicroseconds);
         try {
+            start = StopwatchTimestamp.Now;
             int blockIndex = this.storage.BlockIndex(hash);
             if (blockIndex < 0) {
                 this.misses++;
                 this.MaybeReportHitRate();
+                if (reportPerf)
+                    this.log.LogDebug("miss: {Microseconds:F0}us", start.Elapsed.TotalMicroseconds);
                 return null;
             }
 
-            int read = this.storage.Read(blockIndex: blockIndex, offset: offset32, buffer, cancel);
-            this.hits++;
+            var entryLock = this.blockLocks[blockIndex % this.blockLocks.Length];
+            int read;
+            var readStart = StopwatchTimestamp.Now;
+            await using (await entryLock.ReadLockAsync(cancel)) {
+                this.indexLock.Release();
+                haveLock = false;
+                read = this.storage.Read(blockIndex: blockIndex, offset: offset32, buffer, cancel);
+            }
+
+            if (reportPerf)
+                this.log.LogDebug("reading {Bytes} total: {TotalUS:F0}us search: {ReadUS:F0}us",
+                                  read, start.Elapsed.TotalMicroseconds,
+                                  readStart.Elapsed.TotalMicroseconds);
+            Interlocked.Increment(ref this.hits);
             this.MaybeReportHitRate();
             return read;
         } finally {
-            this.accessLock.Release();
+            if (haveLock)
+                this.indexLock.Release();
         }
     }
 
     public async ValueTask<TimeSpan> FlushAsync(CancellationToken cancel = default) {
-        await this.accessLock.WaitAsync(AccessPriority.ACQUIRER, cancel).ConfigureAwait(false);
+        await this.indexLock.WaitAsync(cancel).ConfigureAwait(false);
+        var lockedBlocks = new List<AsyncReaderWriterLock.Releaser>();
         try {
+            foreach (var blockLock in this.blockLocks)
+                lockedBlocks.Add(await blockLock.WriteLockAsync(cancel));
+
             var start = StopwatchTimestamp.Now;
             await this.storage.FlushAsync(cancel).ConfigureAwait(false);
             return start.Elapsed;
         } finally {
-            this.accessLock.Release();
+            foreach (var blockLock in lockedBlocks)
+                await blockLock.DisposeAsync().ConfigureAwait(false);
+            this.indexLock.Release();
         }
     }
 
@@ -123,6 +173,11 @@ public sealed class BlockCache: IBlockCache, IAsyncDisposable {
             bool evicted = this.evictionStrategy.Access(storage.GetHash(blockIndex), out var __);
             Debug.Assert(!evicted);
         }
+
+        this.blockLocks =
+            new AsyncReaderWriterLock[Math.Min((int)Math.Sqrt(storage.BlockCount), short.MaxValue)];
+        for (int i = 0; i < this.blockLocks.Length; i++)
+            this.blockLocks[i] = new AsyncReaderWriterLock(joinableTaskContext: null);
 
         this.log = log;
     }
